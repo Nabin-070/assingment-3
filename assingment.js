10bee88e0915dd06a6f336073a5745613b25d729
Nabin-070 <nabeen070@gmail.com>
2021-01-22 22:20:14 +0600

    uncommit

--- assingment.js
+++ assingment.js
@@ -1,26 +1,38 @@
-// function kilometerToMeter(kilometer) {
-//    if (kilometer > 0){
-//        var meter = kilometer * 1000;
-//        return meter;
-//    }
-//    else{
-//        return "num is invalid"
-//    }
-// }
-// var total = kilometerToMeter(5);
+// https://github.com/Nabin-070/assingment-3
+
+
+
+// kilometerToMeter
+ 
+ function kilometerToMeter(kilometer) {
+   if (kilometer > 0){
+     var meter = kilometer * 1000;
+       return meter;
+   }
+   else{
+       return "num is invalid"
+   }
+ }
+//  var total = kilometerToMeter(5);
 // console.log(total);
 
 
-// function budgetCalculator (watch, mobile, laptop){
-//       var watch = 5*50;
-//       var mobile = 5*100;
-//       var laptop = 2*500;
-//       var total = watch + mobile + laptop;
 
-//       return total;
-// }
+// budgetCalculator
+
+
+
+function budgetCalculator (watch, mobile, laptop){
+       var watch = 5*50;
+      var mobile = 5*100;
+      var laptop = 2*500;
+      var total = watch + mobile + laptop;
+      return total;
+}
 // var result = budgetCalculator(5,5,2)
 //  console.log(result);
+
+
 
 // megaFriend
 
@@ -35,31 +47,31 @@
      }
      return longest;
  }
-var count = megaFriend (["sangakara", "mahmudullah", "tamim"]);
-console.log(count);
-
-
-
-
-// function hotelCost(days){
-// costamount = 0;
-// if(days <= 10) {
-// costamount = days*100;
-// }
-// else if(days <= 20){
-// var firstTendays = 10*100;
-// var remaining = days - 10;
-// var secondTendays = remaining * 80;
-// costamount = firstTendays + secondTendays;
-// }
-// else{
-//     var firstTendays = 10*100;
-//     var secondTendays = 10*80;
-//     var remaining = days - 20;
-//     var thirdTendays = remaining * 50;
-//     costamount = firstTendays + secondTendays + thirdTendays;
-// }
-// return costamount;
-// }
-// var count = hotelCost(22);
+// var count = megaFriend (["sangakara", "mahmudullah", "tamim"]);
 // console.log(count);
+
+
+
+
+// hotelCost
+
+ function hotelCost(days){
+ costamount = 0;
+ if(days <= 10) {
+ costamount = days*100;
+}
+else if(days <= 20){
+var firstTendays = 10*100;
+var remaining = days - 10;
+var secondTendays = remaining * 80;
+costamount = firstTendays + secondTendays;
+}
+else{
+   var firstTendays = 10*100;
+   var secondTendays = 10*80;
+   var remaining = days - 20;
+   var thirdTendays = remaining * 50;
+  costamount = firstTendays + secondTendays + thirdTendays;
+}
+ return costamount;
+}
