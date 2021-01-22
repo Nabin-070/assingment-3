2f8b1d7c3d9fe225a84c8a8e7c29c3bf851570fe
Nabin-070 <nabeen070@gmail.com>
2021-01-22 22:11:57 +0600

    first commit

---
+++ assingment.js
@@ -0,0 +1,65 @@
+// function kilometerToMeter(kilometer) {
+//    if (kilometer > 0){
+//        var meter = kilometer * 1000;
+//        return meter;
+//    }
+//    else{
+//        return "num is invalid"
+//    }
+// }
+// var total = kilometerToMeter(5);
+// console.log(total);
+
+
+// function budgetCalculator (watch, mobile, laptop){
+//       var watch = 5*50;
+//       var mobile = 5*100;
+//       var laptop = 2*500;
+//       var total = watch + mobile + laptop;
+
+//       return total;
+// }
+// var result = budgetCalculator(5,5,2)
+//  console.log(result);
+
+// megaFriend
+
+ function megaFriend(array){
+     var len = 0;
+     var longest;
+     for (var i = 0; i < array.length; i++){
+         if (array[i].length > len) {
+              len = array[i].length;
+              longest = array[i];
+         }
+     }
+     return longest;
+ }
+var count = megaFriend (["sangakara", "mahmudullah", "tamim"]);
+console.log(count);
+
+
+
+
+// function hotelCost(days){
+// costamount = 0;
+// if(days <= 10) {
+// costamount = days*100;
+// }
+// else if(days <= 20){
+// var firstTendays = 10*100;
+// var remaining = days - 10;
+// var secondTendays = remaining * 80;
+// costamount = firstTendays + secondTendays;
+// }
+// else{
+//     var firstTendays = 10*100;
+//     var secondTendays = 10*80;
+//     var remaining = days - 20;
+//     var thirdTendays = remaining * 50;
+//     costamount = firstTendays + secondTendays + thirdTendays;
+// }
+// return costamount;
+// }
+// var count = hotelCost(22);
+// console.log(count);
